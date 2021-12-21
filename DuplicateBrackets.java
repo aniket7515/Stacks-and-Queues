47bddd94d30f2894d85bd9276867be03d702db83
Aniket <aniketdhokane2071@gmail.com>
2021-12-21 22:42:15 +0530

    Duplicate Brackets

---
+++ DuplicateBrackets.java
@@ -0,0 +1,35 @@
+package StacksAndQueues;
+import java.util.*;
+
+public class DuplicateBrackets {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str=in.nextLine();
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+
+            if(ch==')'){
+                if(st.peek()=='('){
+                    System.out.println(true);
+                    return;
+                }else{
+                    while(st.peek()!='('){
+                        st.pop();
+                    }
+                }
+            }else{
+                st.push(ch);
+            }
+        }
+        System.out.println(false);
+
+    }
+}
+
+
+
+//  (,a,+ ---> Push
+
+// ) if closing bracket occurs pop until you get opening bracket
